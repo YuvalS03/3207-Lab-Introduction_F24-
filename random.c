165422e988c1293fd237db2bcdbc23050414c65d
YuvalS03 <88359507+YuvalS03@users.noreply.github.com>
2024-08-30 21:30:30 -0400

    added random.c and header file

---
+++ random.c
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+char randchar() {
+    char letters[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+    int rand_num = rand() % 26;
+    return letters[rand_num];
+}
